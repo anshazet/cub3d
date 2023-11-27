e000f6c964c9d147a029bdcce0e82fdf6f6a83f5
anshazet <anshazet@gmail.com>
2023-11-27 15:42:36 +0100

    maps

--- src/cub3d.h
+++ src/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ashalagi <<marvin@42.fr>>                  +#+  +:+       +#+        */
+/*   By: ashalagi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 14:15:04 by ashalagi          #+#    #+#             */
-/*   Updated: 2023/11/17 14:19:00 by ashalagi         ###   ########.fr       */
+/*   Updated: 2023/11/27 15:25:17 by ashalagi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,20 @@
 # include <stdio.h>
 #include <stdlib.h>
 
+# define W			119
+# define S			115
+# define A			97
+# define D			100
+
+# define ESC		65307
+
+# define UP			65362
+# define DOWN		65364
+# define LEFT		65361
+# define RIGHT		65363
+
+
+#define screenWidth 640
+#define screenHeight 480
 
 #endif
